93013556d008b993afdb696c8d86eee696630341
liam <lamtruong@panicbear.consulting>
2024-08-29 16:52:54 +0700

    update header + mouse move

--- src/components/pages/Home/Intro/script.jsx
+++ src/components/pages/Home/Intro/script.jsx
@@ -23,38 +23,37 @@ const IntroScript = () => {
 
         tl.to('.home__intro-award', { '--scale-factor': '1', duration: 1, stagger: .03 })
 
-        // let reqID;
+        let reqID;
+        console.log(reqID)
+        const xGetter = gGetter('x');
+        const yGetter = gGetter('y');
+        const xSetter = gSetter('x', 'px');
+        const ySetter = gSetter('y', 'px');
 
-        // const xGetter = gGetter('x');
-        // const yGetter = gGetter('y');
-        // const xSetter = gSetter('x', 'px');
-        // const ySetter = gSetter('y', 'px');
+        let target = document.querySelectorAll('.home__intro-award-logo');
+        let wrap = document.querySelector('.home__intro-awards-wrap');
 
+        const logoMove = () => {
+            if (inView(wrap)) {
+                target.forEach((el) => {
+                    let yMove;
+                    if (document.querySelectorAll('.home__intro-awards:hover').length) {
+                        yMove = getCursor().y - wrap.getBoundingClientRect().top - el.getBoundingClientRect().height / 2;
+                    } else {
+                        yMove = yGetter(target[0])
+                    }
+                    ySetter(el)(lerp(yGetter(target[0]), yMove, .1));
+                })
+            }
+            reqID = requestAnimationFrame(logoMove);
+        }
+        if (reqID == undefined) {reqID = requestAnimationFrame(logoMove)}
 
-        // let target = document.querySelectorAll('.home__intro-award-logo');
-        // let allItems = document.querySelectorAll('.home__intro-award')
-        // let wrap = document.querySelector('.home__intro-awards');
-
-        // let rect = wrap.getBoundingClientRect();
-        // const maxYMove = wrap.offsetHeight / 2;
-
-        // // const logoMove = () => {
-        // //     const init = () => {
-        // //         target.forEach((el) => {
-        // //             let yMove = getCursor().y - (el.getBoundingClientRect().top - maxYMove);
-        // //             let limitBorderYMove = Math.max(Math.min(yMove, maxYMove), -maxYMove) / 2;
-
-        // //             ySetter(el)(lerp(yGetter(target[0]), limitBorderYMove, .55));
-        // //         })
-        // //     }
-        // //     if (inView(wrap)) {
-        // //         init();
-        // //     }
-        // //     reqID = requestAnimationFrame(logoMove);
-        // // }
-        // // reqID = requestAnimationFrame(logoMove)
-
-        onCleanup(() => tl.kill());
+        onCleanup(() => {
+            console.log('cleanup')
+            cancelAnimationFrame(reqID);
+            tl.kill()
+        });
     })
 
     return <div ref={scriptRef} class="divScript"></div>
