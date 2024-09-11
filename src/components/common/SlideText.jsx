731fc637231dab5970bda8be694431accf5b3518
liam <lamtruong@panicbear.consulting>
2024-09-11 15:45:40 +0700

    update footer bg file + slidetxt master tl

--- src/components/common/SlideText.jsx
+++ src/components/common/SlideText.jsx
@@ -4,15 +4,20 @@ import SplitType from "split-type";
 
 function SlideText(props) {
     let slideRef;
-    let tl = [];
+    let tlMaster;
 
     onMount(() => {
         if (!slideRef) return;
+        tlMaster = gsap.timeline({paused: true});
         gsap.set(slideRef.querySelectorAll('.slide-txt-item'), { transformOrigin: props.rootOrigin ? 'center center -.1em !important' : 'center center -.26em !important'});
         slideRef.querySelectorAll('.slide-txt-item').forEach((text, idx) => {
             let dur = 3;
             let ease = 'expo.inOut'
-
+            if (idx == 0) {
+                gsap.set(text, { transform: 'none', autoAlpha: 1 })
+            } else {
+                gsap.set(text, { transform: 'translate3d(0px, 25.5961px, -26.0467px) rotateX(-91deg)', autoAlpha: 0 })
+            }
             let yPercent = { out: 'translate3d(0px, 25.5961px, -26.0467px) rotateX(-91deg)', in: 'translate3d(0px, -25.5961px, -26.0468px) rotateX(91deg)' }
             let tlChild = gsap.timeline({ repeat: -1 });
             if (idx === props.data.length - 1) {
@@ -32,11 +37,12 @@ function SlideText(props) {
                     .to(text, { transform: yPercent.in, autoAlpha: 0, duration: dur, ease: ease })
                     .to(text, { duration: (props.data.length - 2 - idx) * dur})
             }
-            tl.push(tlChild);
+            tlMaster.add(tlChild, 0);
         })
+        tlMaster.play()
     })
-    const handleEnter = () => tl.forEach(el => el.pause());
-    const handleLeave = () => tl.forEach(el => el.resume());
+    const handleEnter = () => tlArr.forEach(el => el.pause());
+    const handleLeave = () => tlArr.forEach(el => el.resume());
     return (
         <>
             <div
