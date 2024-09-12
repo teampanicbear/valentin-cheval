46f711413c1e53ef240477107aff12f205b5aeff
liam <lamtruong@panicbear.consulting>
2024-09-12 16:06:46 +0700

    update

--- src/components/common/SlideText.jsx
+++ src/components/common/SlideText.jsx
@@ -13,36 +13,64 @@ function SlideText(props) {
         slideRef.querySelectorAll('.slide-txt-item').forEach((text, idx) => {
             let dur = 3;
             let ease = 'expo.inOut'
-            if (idx == 0) {
+            let transform = { out: 'translate3d(0px, 25.5961px, -26.0467px) rotateX(-91deg)', in: 'translate3d(0px, -25.5961px, -26.0468px) rotateX(91deg)' }
+            if (idx == props.data.length - 1) {
                 gsap.set(text, { transform: 'none', autoAlpha: 1 })
             } else {
-                gsap.set(text, { transform: 'translate3d(0px, 25.5961px, -26.0467px) rotateX(-91deg)', autoAlpha: 0 })
+                gsap.set(text, { transform: transform.out, autoAlpha: 0 })
             }
-            let yPercent = { out: 'translate3d(0px, 25.5961px, -26.0467px) rotateX(-91deg)', in: 'translate3d(0px, -25.5961px, -26.0468px) rotateX(91deg)' }
-            let tlChild = gsap.timeline({ repeat: -1 });
+            let tlChild = gsap.timeline({repeat: -1});
+            
             if (idx === props.data.length - 1) {
                 tlChild
                     .set(text, { transform: 'none', autoAlpha: 1 })
-                    .to(text, { transform: yPercent.in, autoAlpha: 0, duration: dur, ease: ease }, "<=0")
+                    .to(text, { transform: transform.in, autoAlpha: 0, duration: dur, ease: ease }, "<=0")
                     .to(text, { duration: dur * (idx) - (1 * dur)})
 
-                    .set(text, { transform: yPercent.out, autoAlpha: 0})
+                    .set(text, { transform: transform.out, autoAlpha: 0})
                     .to(text, { transform: 'none', autoAlpha: 1, duration: dur, ease: ease })
-            }
-            else {
+            } else {
                 tlChild
-                    .set(text, { transform: yPercent.out, autoAlpha: 0 })
+                    .set(text, { transform: transform.out, autoAlpha: 0 })
                     .to(text, { duration: dur * idx}, "<=0")
                     .to(text, { transform: 'none', autoAlpha: 1, duration: dur, ease: ease })
-                    .to(text, { transform: yPercent.in, autoAlpha: 0, duration: dur, ease: ease })
+                    .to(text, { transform: transform.in, autoAlpha: 0, duration: dur, ease: ease })
                     .to(text, { duration: (props.data.length - 2 - idx) * dur})
             }
             tlMaster.add(tlChild, 0);
         })
+        if (props.interaction) {
+            // console.log(props.data.length)
+            slideRef.addEventListener('mouseenter', handleEnter);
+            slideRef.addEventListener('mouseleave', handleLeave);
+            slideRef.addEventListener('click', handleClick);
+        }
         tlMaster.play()
     })
-    const handleEnter = () => tlArr.forEach(el => el.pause());
-    const handleLeave = () => tlArr.forEach(el => el.resume());
+    let curIdx = 0;
+    const handleClick = () => {
+        console.log(tlMaster.progress())
+        // tlMaster.pause()
+        curIdx = curIdx + 1
+        let curr = tlMaster.progress()
+        let next = (1/6 * curIdx) - curr
+        console.log(next)
+        gsap.to(tlMaster, { progress: `${next}`, duration: tlMaster.totalDuration() / 3 * next, ease: 'linear', onComplete: () => {
+            
+        }})
+    }
+    const handleEnter = () => {
+        tlMaster.pause()
+        // tlMaster.play()
+        // function update() {
+        //     console.log(tlMaster.progress())
+        //     requestAnimationFrame(update)
+        // }
+        // requestAnimationFrame(update)
+    };
+    const handleLeave = () => {
+        // tlMaster.play()
+    }
     return (
         <>
             <div
